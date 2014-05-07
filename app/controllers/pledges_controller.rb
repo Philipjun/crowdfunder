d1fe464f121ba898be6887063874857ff313f2bf
Jonathan Chang <jonoschang7@gmail.com>
2014-05-06 23:29:01 -0400

    Pledge submit responds to js

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -4,8 +4,16 @@ class PledgesController < ApplicationController
 
 	def create
 		@pledge = Pledge.new(pledge_params)
-		@pledge.save
-		redirect_to projects_path
+		
+    respond_to do |format|
+			if @pledge.save
+        format.html { redirect_to project_path(@project.id), notice: 'Review added.' }
+        format.js {} 
+      else
+        format.html { render 'products/show', alert: 'There was an error.'  }
+        format.js {} 
+      end
+    end
 	end
 
   private
