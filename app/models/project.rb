8a527e2270423c70a5a3b86c52c36ee297c25ea1
Rickytony <karp.richard@gmail.com>
2014-05-08 13:35:41 -0400

    added owner alias and owner_id column to project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ActiveRecord::Base
+  belongs_to :owner, class_name: "User"
   has_many :breakpoints
   has_many :pledges
   accepts_nested_attributes_for :breakpoints, reject_if: :all_blank, allow_destroy: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140507234118) do
+ActiveRecord::Schema.define(version: 20140508173140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20140507234118) do
     t.integer  "pledge_total"
     t.datetime "start_date"
     t.datetime "end_date"
+    t.integer  "owner_id"
   end
 
   create_table "users", force: true do |t|
