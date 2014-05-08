5ac74f2d376e389cb3b7f7e2cdc59159da531a39
Jonathan Chang <jonoschang7@gmail.com>
2014-05-08 15:06:34 -0400

    Add category column to project, update create project form to include category

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140508173140) do
+ActiveRecord::Schema.define(version: 20140508184452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20140508173140) do
     t.datetime "start_date"
     t.datetime "end_date"
     t.integer  "owner_id"
+    t.string   "category"
   end
 
   create_table "users", force: true do |t|
