b217337d6885ac9c65cf60051f273aebb1370438
Jonathan Chang <jonoschang7@gmail.com>
2014-05-09 17:43:47 -0400

    association userse through pledges to project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,7 @@ class Project < ActiveRecord::Base
 	belongs_to :owner, class_name: "User"
 	has_many :breakpoints
 	has_many :pledges
+	has_many :users, :through => :pledges
 	has_many :comments
 	accepts_nested_attributes_for :breakpoints, reject_if: :all_blank, allow_destroy: true
 
