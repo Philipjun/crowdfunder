2807def3a081b20ddff5e0e03898e170ba0f85a0
Philip Jun <inbox@philipjun.com>
2014-06-20 15:29:26 -0400

    created image migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140509204958) do
+ActiveRecord::Schema.define(version: 20140620192854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20140509204958) do
     t.datetime "end_date"
     t.integer  "owner_id"
     t.string   "category"
+    t.string   "image"
   end
 
   create_table "users", force: true do |t|
